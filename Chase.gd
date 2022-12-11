cc8f619dcc743024e9eae826f26cd0f9088228b1
Panda248 <p4nda248@gmail.com>
2022-12-11 15:32:08 -0500

    enemy isnt fast

--- Chase.gd
+++ Chase.gd
@@ -4,8 +4,8 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var motion = Vector2()
-export var velocity = 200;
+export var motion = Vector2()
+export var velocity = 100;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
